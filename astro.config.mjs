a06eee56be093b6f14ab7e416f845191bb28e885
Pieterv24 <9167905+Pieterv24@users.noreply.github.com>
2025-08-04 18:02:48 +0200

    chore: Added sitemap and dynamic robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://static.pieterv24.dev/skydivekompas',
-  integrations: [react()],
+  site: 'https://static.pieterv24.dev/skydivekompas/', // Ensure a trailing slash is present
+  trailingSlash: 'never',
+  integrations: [react(), sitemap()],
 });
