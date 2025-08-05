e637a14adc0bffc700f6ec96e9ed5b9d4b442c39
Pieterv24 <9167905+Pieterv24@users.noreply.github.com>
2025-08-05 14:08:35 +0200

    chore: Added redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://static.pieterv24.dev/skydivekompas/', // Ensure a trailing slash is present
-  trailingSlash: 'never',
-  integrations: [react(), sitemap()]
+  trailingSlash: 'ignore',
+  integrations: [react(), sitemap()],
 });
